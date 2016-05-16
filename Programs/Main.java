35996786bd3d1ee0d13f82812a5a1cf40ef06ad2
Shaily Mishra <shaily.mishra30@gmail.com>
2016-05-16 17:36:15 +0530

    Add files via upload

---
+++ Programs/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+//Class : Main - contains main method
+public class Main 
+{ 
+	 public static void main(String[] args) throws IOException
+	 { 
+		 
+//////////////////////////////////////////////////////////////////////////
+//////////////// Creating objects of Check class //////////////////////
+		 Check c = new Check();
+		 c.CheckText();        //Calling CheckText method
+	 }
+}
